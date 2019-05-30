e094a8e6587e8413852415477f2c17a0df2fe807
Kent Ketter <kent.ketter1@gmail.com>
2019-05-30 14:24:56 -0700

    removed commented out code

--- DSA 401 JAVA/src/main/java/DSA/JAVA/BinarySearch.java
+++ DSA 401 JAVA/src/main/java/DSA/JAVA/BinarySearch.java
@@ -5,7 +5,6 @@ public class BinarySearch {
     public static void main (String[] args){
         BinarySearch ob = new BinarySearch();
         int arr[] = { 2, 3, 4, 10, 40 };
-        int n = arr.length;
         int x = 1;
         int result = ob.binarySearch(arr, x);
         System.out.println(result);
